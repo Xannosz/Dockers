807b7dba2ccd3924d25cd329ce59d180e9cc3009
xannosz <joli.neni@freemail.hu>
2020-11-05 00:21:30 +0100

    backupper docker

--- dockers/scripts/scripts/startBlueQueen.sh
+++ dockers/scripts/scripts/startBlueQueen.sh
@@ -1,1 +1,1 @@
-docker run -v /var/lib/docker/containers:/var/lib/docker/containers -v /persist:/persist -v /var/run/docker.sock:/var/run/docker.sock -p 3500:8888 -d xannosz/bluequeen:latest
+docker run -v /var/lib/docker/containers:/var/lib/docker/containers -v /persist:/persist -v /var/run/docker.sock:/var/run/docker.sock -p 3500:8888 --name bluequeen -d xannosz/bluequeen:latest
