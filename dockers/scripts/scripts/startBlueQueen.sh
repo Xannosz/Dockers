8529988fda04c20796855a139cf318f4f95cf3a3
xannosz <joli.neni@freemail.hu>
2020-11-07 02:34:26 +0100

    Fix blueQueen starter script

--- dockers/scripts/scripts/startBlueQueen.sh
+++ dockers/scripts/scripts/startBlueQueen.sh
@@ -1,1 +1,3 @@
+docker rm -f bluequeen
+docker pull xannosz/bluequeen:latest
 docker run -v /var/lib/docker/containers:/var/lib/docker/containers -v /persist:/persist -v /var/run/docker.sock:/var/run/docker.sock -p 3500:8888 --name bluequeen -d xannosz/bluequeen:latest
